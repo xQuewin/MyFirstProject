77d48687b20a8ff894adad68e76e311d6c37b6ca
xQuewin <ColeQFortune@gmail.com>
2024-08-29 10:41:30 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/*
+Cole Fortune 
+Computer Science Fall 2024
+Due: Sept. 3, 2024
+Lab 1, Lab 1 Introduction to C++
+This lab is a basic demo of the C++ language
+*/
+#include <iostream>
+int main() {
+	std::cout << "Hello Wold!\nBlue" << std::endl;
+		return 0;
+}
